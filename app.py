6f24f4e957f3fe72995260ca6985db60725bb11d
Musfikur Rahaman <shuvo.musfikur@gmail.com>
2024-10-04 19:59:59 +0000

    Personal web with three pages

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("home.html")
+
+@app.route("/experience")
+def experience():
+    return render_template("exp.html")
+
+@app.route( "/projects")
+def projects():
+    return render_template("pj.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
