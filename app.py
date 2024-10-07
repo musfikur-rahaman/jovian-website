5f299de8aeb34158c772fef8865f2744ef68f376
Musfikur Rahaman <shuvo.musfikur@gmail.com>
2024-10-07 19:47:59 +0000

    Jobs excluded

--- app.py
+++ app.py
@@ -22,23 +22,22 @@ JOBS = [{
 
 @app.route("/")
 def home():
-    return render_template("home.html", jobs=JOBS)
+    return render_template("home.html")
 
 
 @app.route("/experience")
 def experience():
-    return render_template("exp.html", jobs=JOBS)
+    return render_template("exp.html")
 
 
 @app.route("/projects")
 def projects():
-    return render_template("pj.html", jobs=JOBS)
+    return render_template("pj.html")
 
 
 @app.route("/api/jobs")
 def list_jobs():
     return jsonify(JOBS)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5002)
